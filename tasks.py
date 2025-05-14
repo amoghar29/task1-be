46315b63b2204f86038e5210a24485a412716062
Amogha R <148464672+amoghar29@users.noreply.github.com>
2025-05-14 19:53:36 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -2,12 +2,9 @@ from celery_app import celery
 from db.redis import r
 
 
-@celery.task()
-def print_tasks(name):
-    print(name)
 
 @celery.task
 def fetch_task_from_redis():
     job = r.lpop("job_queue")
     if job:
-        print_tasks.delay(job)
+        print(job)
